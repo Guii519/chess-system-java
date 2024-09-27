7c822c2be3640c3d987a1d0ac9e35c7aff89bf8b
Guilherme de Freitas Arruda <guilhermefreitas003@gmail.com>
2024-09-27 10:36:33 -0300

    deleting comments

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,7 +3,7 @@ package application;
 import chess.ChessPiece;
 
 public class UI {
-    public static void printBoard(ChessPiece[][] pieces){ // Creating a loop to print the chess interface
+    public static void printBoard(ChessPiece[][] pieces){
         for (int i=0;i< pieces.length;i++){
             System.out.print(8-i + " ");
             for (int j=0; j< pieces.length;j++){
@@ -14,7 +14,7 @@ public class UI {
         System.out.println("  a b c d e f g h");
 
     }
-    public static void printPiece(ChessPiece piece){ // Function to print the parts or "-" if the value of the parts is "null"
+    public static void printPiece(ChessPiece piece){
         if(piece==null){
             System.out.print("-");
         }else {
